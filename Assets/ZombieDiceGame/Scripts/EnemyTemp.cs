8d9b954ffda99b70260c07b2518ece02035779b1
Josh Klope <klope3@msn.com>
2025-11-13 06:47:49 -0600

    Add basic enemy

---
+++ Assets/ZombieDiceGame/Scripts/EnemyTemp.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTemp : MonoBehaviour
+{
+    [SerializeField] private ECM2.Character character;
+    [SerializeField] private Transform playerTrans;
+    [SerializeField] private float chaseDistance;
+    private float timer;
+    private bool wandering;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        Vector3 vecToPlayer = playerTrans.position - transform.position;
+        float distToPlayer = vecToPlayer.magnitude;
+        if (distToPlayer < chaseDistance)
+        {
+            character.SetMovementDirection(vecToPlayer.normalized);
+            return;
+        }
+        else
+        {
+            if (timer > 2)
+            {
+                timer = 0;
+                if (wandering)
+                {
+                    wandering = false;
+                    character.SetMovementDirection(Vector3.zero);
+                } else
+                {
+                    wandering = true;
+                    Vector2 rand = Random.insideUnitCircle;
+                    character.SetMovementDirection(new Vector3(rand.x, 0, rand.y).normalized);
+                }
+            }
+        }
+    }
+}
